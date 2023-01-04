4220b573899fceed25af31a72bbf7eeefc726d6e
Mohamedelaribi <mohamed.elaribi.solicode@gmail.com>
2023-01-05 00:05:49 +0100

    crud and localization

---
+++ Realisation/App/resources/lang/en/message.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'title' => 'English title',
+    'id'=>'id',
+    'firstName'=>'First Name',
+    'lastName'=>'Last Name',
+    'email'=>'Email',
+    'listOfApprentices'=>'List of apprentices',
+    'apprentice'=>'APPRENTICES',
+    'EditApprenticeInformations'=>'EDIT APPRENTICE INFORMATION',
+    'firstNameApprentice'=>'First Name',
+    'lastNameApprentice'=>'Last Name',
+    'emailApprentice'=>'Email',
+    'phoneNumber'=>'PHONE NUMBER',
+    'address'=>'ADDRESS',
+    'action'=>'Action',
+    'submit'=>'Submit'
+];
