5924f58063cb236dfe5db9d80f1ef360b38ee301
Eddaoudi Mouhsine <41871546+Eddaoudi-Mouhsine@users.noreply.github.com>
2023-01-07 18:06:45 +0100

    push

---
+++ Authorisation-Authentication/routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Http\Controllers\LanguageController;
+use App\Http\Controllers\TutorController;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get("/Tutordashboard", [TutorController::class, 'index']);
+Route::get("/addTutor", [TutorController::class, 'tutorForm']);
+Route::post('/TuTorStore', [TutorController::class, 'storingTutor']);
+Route::get("/EditTutor/{id}", [TutorController::class, 'editTutor']);
+Route::post('/EditTutor/{id}', [TutorController::class, 'updateTutor']);
+Route::get("DeleteTutor/{id}", [TutorController::class, 'deleteTutor']);
+
+Route::get('lang/change', [LanguageController::class, 'change'])->name('changeLang');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
